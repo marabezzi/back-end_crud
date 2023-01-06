af16467ad1e0bfbd13c763c1279d826bd572408a
Guilherme Clerice <61362302+marabezzi@users.noreply.github.com>
2023-01-06 16:15:59 -0300

    Remoção Lógica (Soft Delete)

--- src/main/java/io/github/marabezzi/crudbackend/model/Course.java
+++ src/main/java/io/github/marabezzi/crudbackend/model/Course.java
@@ -1,5 +1,8 @@
 package io.github.marabezzi.crudbackend.model;
 
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -18,6 +21,8 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "course")
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?") //Para soft delete
+@Where(clause = "status = 'Ativo'") // mostra apenas com a coluna Ativo
 public class Course {
 	
 	@Id
@@ -37,4 +42,12 @@ public class Course {
 	@Pattern(regexp = "Back-end|Front-end")
 	@Column(name = "category", length = 10, nullable = false)
 	private String category;
+	
+	//Remoção Lógica (Soft Delete)
+	@NotNull
+	@Length(max = 10)
+	@Pattern(regexp = "Ativo|Inativo")
+	@Column(name = "status", length = 10, nullable = false)
+	private String status = "Ativo";
+	
 }
